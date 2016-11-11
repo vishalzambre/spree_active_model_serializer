5770769ff5bfbf14f845e11abe1e11e6de7108c8
Vishal Zambre <vishal.zambre@weboniselab.com>
2016-11-11 11:17:03 +0530

    added spree_api_key to users api

--- app/serializers/spree/user_serializer.rb
+++ app/serializers/spree/user_serializer.rb
@@ -1,6 +1,7 @@
 module Spree
   class UserSerializer < BaseSerializer
     attributes *_helper.user_attributes
+    attributes :spree_api_key
     has_one :bill_address, key: :bill_address, serializer: AddressSerializer
     has_one :ship_address, key: :ship_address, serializer: AddressSerializer
   end
